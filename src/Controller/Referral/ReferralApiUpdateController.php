71bb5970e40863470d7cfda5b01cb2dcd6706c43
Nebalus <56486387+Nebalus@users.noreply.github.com>
2024-02-26 09:23:44 +0100

    dev: .

--- src/Controller/Referral/ReferralApiUpdateController.php
+++ src/Controller/Referral/ReferralApiUpdateController.php
@@ -2,7 +2,17 @@
 
 namespace Nebalus\Ownsite\Controller\Referral;
 
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
 class ReferralApiUpdateController
 {
+    public function __construct()
+    {
+    }
 
+    public function referral(Request $request, Response $response): Response
+    {
+        return $response;
+    }
 }
