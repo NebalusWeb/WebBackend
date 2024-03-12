c74caf4634a4dc646dab7f92a20458d5e76eb4cd
Nebalus <56486387+Nebalus@users.noreply.github.com>
2024-03-12 15:31:59 +0100

    feature-NWS-1: Codebase Cleanup

--- src/Collector/RouteCollector.php
+++ src/Collector/RouteCollector.php
@@ -52,51 +52,23 @@ class RouteCollector
     private function initRoutes(): void
     {
         // Definiert die Route
-        $this->app->group("/api", function (RouteCollectorProxy $group) {
-            $group->get("/account", [TempController::class, "action"]);
-            $group->group("/linktree", function (RouteCollectorProxy $group) {
-                $group->post("/create", [LinktreeApiCreateController::class, "action"]);
-                $group->get("/read", [LinktreeApiReadController::class, "action"]);
-                $group->post("/update", [LinktreeApiUpdateController::class, "action"]);
-                $group->delete("/delete", [LinktreeApiDeleteController::class, "action"]);
+        $this->app->get("/account", [TempController::class, "action"]);
+        $this->app->group("/linktree", function (RouteCollectorProxy $group) {
+            $group->post("/create", [LinktreeApiCreateController::class, "action"]);
+            $group->get("/read", [LinktreeApiReadController::class, "action"]);
+            $group->post("/update", [LinktreeApiUpdateController::class, "action"]);
+            $group->delete("/delete", [LinktreeApiDeleteController::class, "action"]);
+        });
+        $this->app->group("/referral", function (RouteCollectorProxy $group) {
+            $group->post("/create", [ReferralApiCreateController::class, "action"]);
+            $group->get("/read", [ReferralApiReadController::class, "action"]);
+            $group->post("/update", [ReferralApiUpdateController::class, "action"]);
+            $group->delete("/delete", [ReferralApiDeleteController::class, "action"]);
+        });
+        $this->app->group("/game", function (RouteCollectorProxy $group) {
+            $group->group("/cosmoventure", function (RouteCollectorProxy $group) {
+                $group->get("/version", [TempController::class, "action"]);
             });
-            $group->group("/referral", function (RouteCollectorProxy $group) {
-                $group->post("/create", [ReferralApiCreateController::class, "action"]);
-                $group->get("/read", [ReferralApiReadController::class, "action"]);
-                $group->post("/update", [ReferralApiUpdateController::class, "action"]);
-                $group->delete("/delete", [ReferralApiDeleteController::class, "action"]);
-            });
-            $group->group("/game", function (RouteCollectorProxy $group) {
-                $group->group("/cosmoventure", function (RouteCollectorProxy $group) {
-                    $group->get("/version", [TempController::class, "action"]);
-                });
-            });
-        })->add(JsonValidatorMiddleware::class);
-
-        $this->app->group("/u", function (RouteCollectorProxy $group) {
-            $group->get("/{username}", [TempController::class, "action"]);
         });
-
-        $this->app->group("/account", function (RouteCollectorProxy $group) {
-            $group->get("/register", [TempController::class, "action"]);
-            $group->get("/login", [TempController::class, "action"]);
-            $group->get("/dashboard", [TempController::class, "action"]);
-        });
-
-        $this->app->group("/terms", function (RouteCollectorProxy $group) {
-            $group->get("/privacy", [TempController::class, "action"]);
-        });
-
-        $this->app->group("/projects", function (RouteCollectorProxy $group) {
-            $group->get("/mandelbrot", [TempController::class, "action"]);
-            $group->get("/oriri", [TempController::class, "action"]);
-            $group->get("/cosmoventure", [TempController::class, "action"]);
-            $group->get("/melody", [TempController::class, "action"]);
-        });
-
-        $this->app->get("/", [HomeController::class, "action"]);
-        $this->app->get("/docs", [DocsController::class, "action"]);
-        $this->app->get("/linktree", [LinktreeController::class, "action"]);
-        $this->app->get("/ref", [ReferralController::class, "action"]);
     }
 }
