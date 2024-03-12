e905b09c9b82a518566a0f0e206dc486d4d35385
Nebalus <56486387+Nebalus@users.noreply.github.com>
2024-03-12 13:21:40 +0100

    dev-temp: .

--- src/Factory/PdoFactory.php
+++ src/Factory/PdoFactory.php
@@ -9,7 +9,7 @@ class PdoFactory
 
     public function build(): PDO
     {
-        $dsn = 'mysql:host=mysql-db:3306;dbname=main';
+        $dsn = 'mysql:host=mysql:3306;dbname=main';
         
         return new PDO($dsn, getenv("MYSQL_USER"), getenv("MYSQL_PASSWORD"));
     }
