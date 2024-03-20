c4cf838bd2228a378114549752e34ac72ff9bb35
Nebalus <56486387+Nebalus@users.noreply.github.com>
2024-03-20 12:06:20 +0100

    feature-NWS-2: Added new table in db 'auth'

--- docker/mysql/initdb.sql
+++ docker/mysql/initdb.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: mysql
--- Generation Time: Mar 17, 2024 at 10:53 PM
+-- Generation Time: Mar 20, 2024 at 11:05 AM
 -- Server version: 8.3.0
--- PHP Version: 8.2.16
+-- PHP Version: 8.2.15
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -45,6 +45,17 @@ INSERT INTO `accounts` (`id`, `creationdate`, `username`, `passwd_hash`, `passwd
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `auth`
+--
+
+CREATE TABLE `auth` (
+                        `nonce` int NOT NULL,
+                        `expire_at` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `linktrees`
 --
 
@@ -53,7 +64,7 @@ CREATE TABLE `linktrees` (
                              `accountid` int NOT NULL COMMENT 'The ID of the account that owns this entry',
                              `description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci COMMENT 'This text is shown as the description',
                              `creationdate` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'The creation date of this entry',
-                             `viewcount` int NOT NULL DEFAULT '0' COMMENT 'The amount of times this entry was accessed'
+                             `count` int NOT NULL DEFAULT '0' COMMENT 'The amount of times this entry was accessed'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -81,7 +92,7 @@ CREATE TABLE `referrals` (
                              `accountid` int NOT NULL COMMENT 'The ID of the account that owns this entry',
                              `code` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT 'A unique code that is used for /ref?q=code',
                              `pointer` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '/' COMMENT 'Points to the Final URL (''/'' is the root path from the aktual webserver)',
-                             `viewcount` int NOT NULL DEFAULT '0' COMMENT 'The amount of times this referral entry has been accessed',
+                             `count` int NOT NULL DEFAULT '0' COMMENT 'The amount of times this referral entry has been accessed',
                              `creationdate` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'The creation date of this entry',
                              `enabled` tinyint(1) NOT NULL DEFAULT '1' COMMENT 'Defines if this referral is enabled'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
@@ -90,24 +101,11 @@ CREATE TABLE `referrals` (
 -- Dumping data for table `referrals`
 --
 
-INSERT INTO `referrals` (`id`, `accountid`, `code`, `pointer`, `viewcount`, `creationdate`, `enabled`) VALUES
-                                                                                                           (1, 1, 'TEST', '/', 0, '2024-02-25 00:00:00', 1),
-                                                                                                           (3, 1, 'TEST1', '/', 0, '2024-02-25 00:00:00', 1),
-                                                                                                           (5, 1, 'TEST42', '/', 0, '2024-02-27 11:04:20', 1),
-                                                                                                           (6, 1, '42', '/', 0, '2024-02-28 21:30:24', 1);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tokens`
---
-
-CREATE TABLE `tokens` (
-                          `id` int NOT NULL COMMENT 'The ID of this entry (Primary Key)',
-                          `creationdate` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'The creation date of this entry',
-                          `expiredate` datetime NOT NULL COMMENT 'The expire date of this entry',
-                          `token` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+INSERT INTO `referrals` (`id`, `accountid`, `code`, `pointer`, `count`, `creationdate`, `enabled`) VALUES
+                                                                                                       (1, 1, 'TEST', '/', 0, '2024-02-25 00:00:00', 1),
+                                                                                                       (3, 1, 'TEST1', '/', 0, '2024-02-25 00:00:00', 1),
+                                                                                                       (5, 1, 'TEST42', '/', 0, '2024-02-27 11:04:20', 1),
+                                                                                                       (6, 1, '42', '/', 0, '2024-02-28 21:30:24', 1);
 
 --
 -- Indexes for dumped tables
@@ -124,7 +122,7 @@ ALTER TABLE `accounts`
 --
 ALTER TABLE `linktrees`
     ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `account` (`accountid`);
+    ADD UNIQUE KEY `account` (`accountid`);
 
 --
 -- Indexes for table `linktree_entrys`
@@ -137,13 +135,7 @@ ALTER TABLE `linktree_entrys`
 --
 ALTER TABLE `referrals`
     ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `refcode` (`code`,`accountid`) USING BTREE;
-
---
--- Indexes for table `tokens`
---
-ALTER TABLE `tokens`
-    ADD PRIMARY KEY (`id`);
+    ADD UNIQUE KEY `refcode` (`code`,`accountid`) USING BTREE;
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -172,12 +164,6 @@ ALTER TABLE `linktree_entrys`
 --
 ALTER TABLE `referrals`
     MODIFY `id` int NOT NULL AUTO_INCREMENT COMMENT 'The ID of this entry (Primary Key)', AUTO_INCREMENT=7;
-
---
--- AUTO_INCREMENT for table `tokens`
---
-ALTER TABLE `tokens`
-    MODIFY `id` int NOT NULL AUTO_INCREMENT COMMENT 'The ID of this entry (Primary Key)';
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
