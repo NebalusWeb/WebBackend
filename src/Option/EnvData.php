2c0daa2cb396fd39ba7b1213d18208bb7c9f0cb8
Nebalus <56486387+Nebalus@users.noreply.github.com>
2024-04-15 10:24:18 +0200

    feature-NWS-2: Code Cleanup

--- src/Option/EnvData.php
+++ src/Option/EnvData.php
@@ -7,20 +7,28 @@ class EnvData
     private bool $isProduction;
     private bool $isDevelopment;
     private string $jwtSecret;
+    private string $mysqlPasswd;
+    private string $mysqlHost;
+    private string $mysqlDbName;
+    private string $mysqlUser;
 
     public function __construct()
     {
         $this->isProduction = strtolower(getenv("APP_ENV")) === "production";
         $this->isDevelopment = strtolower(getenv("APP_ENV")) === "development";
         $this->jwtSecret = getenv("JWT_SECRET");
+        $this->mysqlPasswd = getenv("MYSQL_PASSWORD");
+        $this->mysqlHost = getenv("MYSQL_HOST");
+        $this->mysqlDbName = getenv("MYSQL_DBNAME");
+        $this->mysqlUser = getenv("MYSQL_USER");
     }
 
-    public function isProduction()
+    public function isProduction(): bool
     {
         return $this->isProduction;
     }
 
-    public function isDevelopment()
+    public function isDevelopment(): bool
     {
         return $this->isDevelopment;
     }
@@ -28,5 +36,25 @@ class EnvData
     public function getJwtSecret(): string
     {
         return $this->jwtSecret;
+    }
+
+    public function getMySqlPasswd(): string
+    {
+        return $this->mysqlPasswd;
+    }
+
+    public function getMySqlHost(): string
+    {
+        return $this->mysqlHost;
+    }
+
+    public function getMySqlDbName(): string
+    {
+        return $this->mysqlDbName;
+    }
+
+    public function getMySqlUser(): string
+    {
+        return $this->mysqlUser;
     }
 }
