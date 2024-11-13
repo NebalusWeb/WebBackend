f7f809a49d9db003d20d396a2094c62f1c0ad432
nebalus <coding@nebalus.dev>
2024-11-14 00:32:46 +0100

    Code Refactor

--- src/Repository/MySqlReferralRepository.php
+++ src/Repository/MySqlReferralRepository.php
@@ -8,6 +8,8 @@ use DateMalformedStringException;
 use Nebalus\Webapi\Value\ID;
 use Nebalus\Webapi\Value\Referral\Referral;
 use Nebalus\Webapi\Value\Referral\ReferralCode;
+use Nebalus\Webapi\Value\Referral\ReferralPointer;
+use Nebalus\Webapi\Value\Referral\Referrals;
 use PDO;
 
 readonly class MySqlReferralRepository
@@ -17,23 +19,23 @@ readonly class MySqlReferralRepository
     ) {
     }
 
-    public function createReferral(ID $userId, ReferralCode $code, string $pointer, bool $disabled = true): bool
+    public function insertReferral(ID $userId, ReferralCode $code, ReferralPointer $pointer, bool $disabled = true): bool
     {
-        $sql = "INSERT INTO `referrals`(`user_id`, `code`, `pointer`, `disabled`) VALUES (:user_id, :code, :pointer, :disabled)";
+        $sql = "INSERT INTO referrals(user_id, code, pointer, disabled) VALUES (:user_id, :code, :pointer, :disabled)";
 
         $stmt = $this->pdo->prepare($sql);
         $stmt->bindValue(':user_id', $userId->asInt());
         $stmt->bindValue(':code', $code->asString());
-        $stmt->bindValue(':pointer', $pointer);
+        $stmt->bindValue(':pointer', $pointer->asString());
         $stmt->bindValue(':disabled', $disabled);
         $stmt->execute();
 
         return $stmt->rowCount() === 1;
     }
 
-    public function createReferralClickEntry(ID $referralId): bool
+    public function insertReferralClickEntry(ID $referralId): bool
     {
-        $sql = "INSERT INTO `referral_analytics_clicks`(`referral_id`) VALUES (:referral_id)";
+        $sql = "INSERT INTO referral_analytics_clicks(referral_id) VALUES (:referral_id)";
 
         $stmt = $this->pdo->prepare($sql);
         $stmt->bindValue(':referral_id', $referralId->asInt());
@@ -42,28 +44,78 @@ readonly class MySqlReferralRepository
         return $stmt->rowCount() === 1;
     }
 
-    public function deleteReferralById(ID $id)
+    public function deleteReferralById(ID $referralId): bool
     {
+        $sql = "DELETE FROM referrals WHERE referral_id = :referral_id";
+
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindValue(':referral_id', $referralId->asInt());
+        $stmt->execute();
+
+        return $stmt->rowCount() === 1;
     }
 
-    public function deleteReferralByCode(string $code)
+    public function deleteReferralByCode(ReferralCode $code): bool
     {
+        $sql = "DELETE FROM referrals WHERE code = :code";
+
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindValue(':code', $code->asString());
+        $stmt->execute();
+
+        return $stmt->rowCount() === 1;
     }
 
     public function updateReferral()
     {
     }
 
-    public function getReferralById(ID $id)
+    /**
+     * @throws DateMalformedStringException
+     */
+    public function getReferralsFromUserId(ID $userId): Referrals
     {
+        $sql = "SELECT * FROM referrals WHERE user_id = :user_id";
+
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindValue(':user_id', $userId->asInt());
+        $stmt->execute();
+
+        $data = [];
+
+        while ($row = $stmt->fetch()) {
+            $data[] = Referral::fromMySQL($row);
+        }
+
+        return Referrals::fromArray(...$data);
     }
 
     /**
      * @throws DateMalformedStringException
      */
-    public function getReferralByCode(ReferralCode $code): ?Referral
+    public function findReferralById(ID $id): ?Referral
     {
-        $sql = "SELECT * FROM `referrals` WHERE BINARY `code` = :code";
+        $sql = "SELECT * FROM referrals WHERE referral_id = :referral_id";
+
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindValue(':referral_id', $id->asInt());
+        $stmt->execute();
+
+        $data = $stmt->fetch();
+
+        if (!$data) {
+            return null;
+        }
+
+        return Referral::fromMySql($data);
+    }
+
+    /**
+     * @throws DateMalformedStringException
+     */
+    public function findReferralByCode(ReferralCode $code): ?Referral
+    {
+        $sql = "SELECT * FROM referrals WHERE BINARY code = :code";
 
         $stmt = $this->pdo->prepare($sql);
         $stmt->bindValue(':code', $code->asString());
