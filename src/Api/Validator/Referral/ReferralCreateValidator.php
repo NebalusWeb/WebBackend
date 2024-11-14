ced5ef5233a151ef84302eca8caa874646adbe18
Nebalus <56486387+Nebalus@users.noreply.github.com>
2024-11-14 21:50:55 +0100

    master: .

---
+++ src/Api/Validator/Referral/ReferralCreateValidator.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Nebalus\Webapi\Api\Validator\Referral;
+
+use Nebalus\Webapi\Api\Validator\AbstractValidator;
+
+class ReferralCreateValidator extends AbstractValidator
+{
+}
---
+++ src/Exception/ApiJwtException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Nebalus\Webapi\Exception;
+
+use Nebalus\Webapi\Exception\ApiException;
+
+class ApiJwtException extends ApiException
+{
+}
