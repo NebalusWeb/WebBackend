a51f958931b2db00a7b3b084aab50ad28d5ef93f
Nebalus <56486387+Nebalus@users.noreply.github.com>
2024-12-16 13:49:13 +0100

    communication: Bugfix

--- docker/mysql/initdb.sql
+++ docker/mysql/initdb.sql
@@ -149,7 +149,8 @@ CREATE TABLE `form_entrys` (
                                `form_entry_id` int UNSIGNED NOT NULL,
                                `form_id` int UNSIGNED NOT NULL,
                                `type` enum('timepicker','textinput','checkbox') NOT NULL,
-                               `name` varchar(64) NOT NULL
+                               `name` varchar(64) NOT NULL,
+                               `required` bit(1) NOT NULL DEFAULT b'0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
