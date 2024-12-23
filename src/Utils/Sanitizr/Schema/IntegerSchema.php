3b6ad3126aa773a27b0bed9308dcc126f96e21dc
Nebalus <56486387+Nebalus@users.noreply.github.com>
2024-12-23 13:09:35 +0100

    sanitizr: Editing Schema

--- src/Utils/Sanitizr/Schema/IntegerSchema.php
+++ src/Utils/Sanitizr/Schema/IntegerSchema.php
@@ -2,13 +2,53 @@
 
 namespace Nebalus\Webapi\Utils\Sanitizr\Schema;
 
+use Nebalus\Webapi\Utils\Sanitizr\Exception\SanitizValidationException;
 use Nebalus\Webapi\Utils\Sanitizr\Schema\AbstractSchema;
 
 class IntegerSchema extends AbstractSchema
 {
+    private $min;
+    private $max;
 
-    protected function parseValue($value): mixed
+    public function min(int $min)
     {
-        // TODO: Implement parseValue() method.
+        $this->min = $min;
+        return $this;
+    }
+
+    public function max(int $max)
+    {
+        $this->max = $max;
+        return $this;
+    }
+
+    public function positive()
+    {
+        return $this->min(1);
+    }
+
+    public function negative()
+    {
+        return $this->max(-1);
+    }
+
+    /**
+     * @throws SanitizValidationException
+     */
+    protected function parseValue($value): int
+    {
+        if (! is_numeric($value)) {
+            throw new SanitizValidationException('Not a numeric value');
+        }
+        if (is_string($value)) {
+            throw new SanitizValidationException('Numeric value is a string');
+        }
+        if (! is_null($this->min) && $value < $this->min) {
+            throw new SanitizValidationException('Value is too small');
+        }
+        if (! is_null($this->max) && $this->max < $value) {
+            throw new SanitizValidationException('Value is too big');
+        }
+        return $value;
     }
 }
