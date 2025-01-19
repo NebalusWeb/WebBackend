b4dd712b5200780a4761b7bf95ddd5fac8ae8089
nebalus <coding@nebalus.dev>
2025-01-19 19:51:52 +0100

    MySqlRepositories Refactor

--- src/Api/Module/Referral/Analytics/ClickHistory/ClickHistoryReferralService.php
+++ src/Api/Module/Referral/Analytics/ClickHistory/ClickHistoryReferralService.php
@@ -2,15 +2,21 @@
 
 namespace Nebalus\Webapi\Api\Module\Referral\Analytics\ClickHistory;
 
+use Nebalus\Webapi\Repository\ReferralRepository\MySqlReferralRepository;
 use Nebalus\Webapi\Value\Internal\Result\ResultInterface;
 use Nebalus\Webapi\Value\User\User;
 
-class ClickHistoryReferralService
+readonly class ClickHistoryReferralService
 {
+    public function __construct(
+        private MySQlReferralRepository $referralRepository
+    ) {
+    }
 
 
     public function execute(ClickHistoryReferralValidator $validator, User $user): ResultInterface
     {
+        $this->referralRepository->getReferralClicksFromRange($user->getUserId(), $validator->getReferralCode(), $validator->getRange());
 
         return ClickHistoryReferralView::render();
     }
