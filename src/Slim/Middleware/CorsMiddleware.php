8f085016616aebf888db1a425c07b7fe43776401
Nebalus <coding@nebalus.dev>
2025-03-22 23:56:08 +0100

    Env rename to config

--- src/Slim/Middleware/CorsMiddleware.php
+++ src/Slim/Middleware/CorsMiddleware.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Nebalus\Webapi\Slim\Middleware;
 
-use Nebalus\Webapi\Option\GeneralEnv;
+use Nebalus\Webapi\Config\GeneralConfig;
 use Override;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
@@ -16,7 +16,7 @@ readonly class CorsMiddleware implements MiddlewareInterface
 {
     public function __construct(
         private App $app,
-        private GeneralEnv $env
+        private GeneralConfig $env
     ) {
     }
 
