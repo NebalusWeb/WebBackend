3e2460172840c290cc29d0e460c86728882bb2e8
nebalus <coding@nebalus.dev>
2025-04-07 13:00:17 +0200

    Added Sql querys

--- src/Value/User/AccessControl/Privilege/PrivilegeNode.php
+++ src/Value/User/AccessControl/Privilege/PrivilegeNode.php
@@ -4,5 +4,18 @@ namespace Nebalus\Webapi\Value\User\AccessControl\Privilege;
 
 class PrivilegeNode
 {
+    private function __construct(
+        private string $node
+    ) {
+    }
 
+    public static function from(string $node): self
+    {
+        return new self($node);
+    }
+
+    public function asString(): string
+    {
+        return $this->node;
+    }
 }
