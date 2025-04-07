6c7e53e26e2ca69dc4245365ec51362b127e63e7
nebalus <coding@nebalus.dev>
2025-04-07 13:00:17 +0200

    Added Sql querys

--- src/Repository/PrivilegesRepository/MySqlPrivilegesRepository.php
+++ src/Repository/PrivilegesRepository/MySqlPrivilegesRepository.php
@@ -2,7 +2,39 @@
 
 namespace Nebalus\Webapi\Repository\PrivilegesRepository;
 
+use Nebalus\Webapi\Exception\ApiException;
+use Nebalus\Webapi\Exception\ApiInvalidArgumentException;
+use Nebalus\Webapi\Value\User\AccessControl\Privilege\Privilege;
+use Nebalus\Webapi\Value\User\AccessControl\Privilege\PrivilegeNode;
+use PDO;
+
 class MySqlPrivilegesRepository
 {
+    public function __construct(
+        private PDO $pdo,
+    ) {
+    }
 
+    /**
+     * @throws ApiInvalidArgumentException
+     * @throws ApiException
+     */
+    public function findPrivilegeByNode(PrivilegeNode $node): ?Privilege
+    {
+        $sql = <<<SQL
+            SELECT * FROM privileges WHERE node = :node
+        SQL;
+
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindValue("node", $node->asString());
+        $stmt->execute();
+
+        $data = $stmt->fetch();
+
+        if (!$data) {
+            return null;
+        }
+
+        return Privilege::fromArray($data);
+    }
 }
--- src/Value/User/AccessControl/Privilege/Privilege.php
+++ src/Value/User/AccessControl/Privilege/Privilege.php
@@ -2,6 +2,9 @@
 
 namespace Nebalus\Webapi\Value\User\AccessControl\Privilege;
 
+use Nebalus\Webapi\Exception\ApiException;
+use Nebalus\Webapi\Exception\ApiInvalidArgumentException;
+
 class Privilege
 {
     public function __construct(
@@ -21,6 +24,19 @@ class Privilege
             $node,
             $description
         );
+    }
+
+    /**
+     * @throws ApiInvalidArgumentException
+     * @throws ApiException
+     */
+    public static function fromArray(array $value): self
+    {
+        $id = PrivilegeId::from($value['privilege_id']);
+        $node = PrivilegeNode::from($value['node']);
+        $description = PrivilegeDescription::from($value['description']);
+
+        return new self($id, $node, $description);
     }
 
     public function getPrivilegeId(): PrivilegeId
