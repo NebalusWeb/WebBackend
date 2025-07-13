81a0cce5113a62750c0535edeaa7a27366f9ba71
Nebalus <coding@nebalus.dev>
2025-07-13 22:30:56 +0200

    Small Changes

---
+++ src/Api/Module/Referral/GetAll/GetAllReferralValidator.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Nebalus\Webapi\Api\Module\Referral\GetAll;
+
+use Nebalus\Webapi\Api\AbstractValidator;
+
+class GetAllReferralValidator extends AbstractValidator
+{
+
+    /**
+     * @inheritDoc
+     */
+    protected function onValidate(array $bodyData, array $queryParamsData, array $pathArgsData): void
+    {
+        // TODO: Implement onValidate() method.
+    }
+}
